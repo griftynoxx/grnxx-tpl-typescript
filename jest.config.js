4d7985854826168bc131a5cdf4347bff6dd4e662
Griftynoxx <griftynoxx@gmail.com>
2020-01-21 12:02:31 +0100

    Remove  unnecessary configuration

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,7 @@
 module.exports = {
-  moduleFileExtensions: ["ts", "tsx", "js", "json"],  
+  roots: ["<rootDir>/src"],  
+  testMatch: ["**/test/**/*.+(ts|tsx|js)"],
   transform: {
     "^.+\\.(ts|tsx)$": "ts-jest"
-  },
-  globals: {
-    "ts-jest": {
-      tsConfig: "tsconfig.json"
-    }
-  },
-  testMatch: ["**/src/test/**/*.+(ts|tsx|js)"],
-  testPathIgnorePatterns: ["/node_modules/", "/dist/", "/lib/"],
-  verbose: true
+  }    
 };
